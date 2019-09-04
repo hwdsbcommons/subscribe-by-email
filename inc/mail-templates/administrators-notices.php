57c60db7edf8f27710596a7a70759244718e9177
Jared Bennett <jarbenne@hwdsb.on.ca>
2019-09-04 17:23:58 -0400

    Some language fixes in the admin emails

--- inc/mail-templates/administrators-notices.php
+++ inc/mail-templates/administrators-notices.php
@@ -32,7 +32,7 @@ class Incsub_Subscribe_By_Email_Administrators_Subscribed_Notice_Template extend
 		$subscribers_link = Incsub_Subscribe_By_Email::$admin_subscribers_page->get_permalink();
 
 		$message  = _x( 'Dear user,', 'Administrator notice sent when a new user is subscribed', INCSUB_SBE_LANG_DOMAIN ) . "\r\n\r\n";
-		$message  .= sprintf ( __( 'A new user (%s) has confirmed his subscription to your subscribers list.', INCSUB_SBE_LANG_DOMAIN ), $this->subscriber_email ) . "\r\n\r\n";
+		$message  .= sprintf ( __( 'A new user (%s) has confirmed their subscription.', INCSUB_SBE_LANG_DOMAIN ), $this->subscriber_email ) . "\r\n\r\n";
 		$message  .= __( 'Click this link to manage your subscribers:', INCSUB_SBE_LANG_DOMAIN ) . "\r\n";
 		$message  .= $subscribers_link . "\r\n\r\n";
 		$message  .= _x( 'Regards,', 'Administrator notice sent when a new user is subscribed', INCSUB_SBE_LANG_DOMAIN ) . "\r\n";
@@ -54,7 +54,7 @@ class Incsub_Subscribe_By_Email_Administrators_Unsubscribed_Notice_Template exte
 		$subscribers_link = Incsub_Subscribe_By_Email::$admin_subscribers_page->get_permalink();
 
 		$message  = _x( 'Dear user,', 'Administrator notice sent when a new user is unsubscribed', INCSUB_SBE_LANG_DOMAIN ) . "\r\n\r\n";
-		$message  .= sprintf ( __( 'A new user (%s) has removed from your subcribers list.', INCSUB_SBE_LANG_DOMAIN ), $this->subscriber_email ) . "\r\n\r\n";
+		$message  .= sprintf ( __( 'A user (%s) has removed themselves from your subscribers list.', INCSUB_SBE_LANG_DOMAIN ), $this->subscriber_email ) . "\r\n\r\n";
 		$message  .= __( 'Click this link to manage your subscribers:', INCSUB_SBE_LANG_DOMAIN ) . "\r\n";
 		$message  .= $subscribers_link . "\r\n\r\n";
 		$message  .= _x( 'Regards,', 'Administrator notice sent when a new user is unsubscribed', INCSUB_SBE_LANG_DOMAIN ) . "\r\n";
